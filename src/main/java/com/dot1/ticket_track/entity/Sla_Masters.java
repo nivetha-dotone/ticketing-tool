5956e16b5d8c91f66078ab2f960baabc6caf8d6d
mayurpatil29 <125718899+mayurpatil29@users.noreply.github.com>
2025-05-07 12:23:32 +0530

    Push to try

--- src/main/java/com/dot1/ticket_track/entity/Sla_Masters.java
+++ src/main/java/com/dot1/ticket_track/entity/Sla_Masters.java
@@ -1,5 +1,44 @@
 package com.dot1.ticket_track.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Sla_Masters")
 public class Sla_Masters {
+
+    @Id
+    @Column(name = "slaId", nullable = false, length = 9, unique = true)
+    private Long slaId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "tickettype")
+    private  mGeneralMaster tickettype;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "ticketlevel")
+    private  mGeneralMaster ticketlevel;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "priority")
+    private mGeneralMaster priority;
+
+ // to store a timeing only for further calculation
+
+    @Column(name = "sla_time")
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime slaTime;
+
+
+
 }
